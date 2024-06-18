d4966833b30e8fc649934ee63ad9013ef41176d1
Tanush Anand <tanushanand225@gmail.com>
2024-06-18 21:29:03 +0530

    changed work to function
    Co-authored-by: Adit K <BTW-AAK@users.noreply.github.com>

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hi there, does this work!</Text>
+      <Text>Hi there, does this function!</Text>
       <StatusBar style="auto" />
     </View>
   );
